1965ac7728d3427434178a5a7b6447415c8f9a46
Kilian Koeltzsch <me@kilian.io>
2016-07-02 17:05:04 +0200

    refactor swift hamming

--- swift/hamming/Hamming.swift
+++ swift/hamming/Hamming.swift
@@ -1,6 +1,6 @@
 class Hamming {
     static func compute(_ base: String, against: String) -> Int? {
         guard base.characters.count == against.characters.count else { return nil }
-        return base.characters.indices.map { base[$0] != against[$0] ? 1 : 0 }.reduce(0, combine: +)
+        return zip(base.characters, against.characters).filter(!=).count
     }
 }
