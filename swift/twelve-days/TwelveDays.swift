699ad81b59c16ee27e45295e6a36d8742938f6c9
Kilian Koeltzsch <me@kilian.io>
2016-07-09 17:27:28 +0200

    improve swift twelve-days
    looks like I accidentally went the easy route earlier
    and should actually be generating the song instead of
    directly storing all the verses, oops^^

--- swift/twelve-days/TwelveDays.swift
+++ swift/twelve-days/TwelveDays.swift
@@ -1,35 +1,43 @@
 class TwelveDaysSong {
-    static func verse(_ verse: Int) -> String {
-        guard verse > 0 else { return "" }
-        return verses[verse-1]
+    static func verse(_ idx: Int) -> String {
+        guard idx > 0 else { return "" }
+        var verse = "On the \(songData[idx-1].0) day of Christmas my true love gave to me,"
+        for i in (0..<idx).reversed() {
+            if i == 0 && idx > 1 {
+                verse.append(" and")
+            }
+            verse.append(songData[i].1)
+        }
+        verse.append("\n")
+        return verse
     }
 
     static func verses(_ from: Int, _ to: Int) -> String {
-        guard from > 0 && to <= verses.count else { return "" }
-        let song = verses
-            .filter{return ((from-1)...(to-1)).contains(verses.index(of: $0)!)}
+        guard from > 0 && to <= 12 else { return "" }
+        let verses = (from...to)
+            .map{verse($0)}
             .joined(separator: "\n")
-        return "\(song)\n"
-        // Returning song without the extra `\n` at the end makes a lot
+        return "\(verses)\n"
+        // Returning verses without the extra `\n` at the end makes a lot
         // more sense, but the tests are expecting it for some reason...
     }
 
     static func sing() -> String {
-        return verses(1, verses.count)
+        return verses(1, songData.count)
     }
 
-    private static let verses = [
-        "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n",
-        "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
-        "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+    private static let songData = [
+        ("first", " a Partridge in a Pear Tree."),
+        ("second", " two Turtle Doves,"),
+        ("third", " three French Hens,"),
+        ("fourth", " four Calling Birds,"),
+        ("fifth", " five Gold Rings,"),
+        ("sixth", " six Geese-a-Laying,"),
+        ("seventh", " seven Swans-a-Swimming,"),
+        ("eighth", " eight Maids-a-Milking,"),
+        ("ninth", " nine Ladies Dancing,"),
+        ("tenth", " ten Lords-a-Leaping,"),
+        ("eleventh", " eleven Pipers Piping,"),
+        ("twelfth", " twelve Drummers Drumming,")
     ]
 }
