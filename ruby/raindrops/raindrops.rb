a83d161d3a826edec269b4d98b972560bb0263ec
Kilian Koeltzsch <me@kilian.io>
2016-08-31 17:52:07 +0200

    improve ruby raindrops
    - remove unecessary return
    - rename factors to divisors
    - reformat hash literal to be more readable

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,10 +1,14 @@
 class Raindrops
 
-  RULES = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
+  RULES = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong',
+  }
 
   def self.convert(num)
-    factors = RULES.keys.select { |f| num % f == 0 }
-    return factors.empty? ? num.to_s : factors.map { |n| RULES[n] }.join
+    divisors = RULES.keys.select { |f| num % f == 0 }
+    divisors.empty? ? num.to_s : divisors.map { |n| RULES[n] }.join
   end
 end
 
