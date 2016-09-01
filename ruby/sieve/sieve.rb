558902d02f704f8a6483acb7c50e31b033d3e031
Kilian Koeltzsch <me@kilian.io>
2016-09-01 11:57:20 +0200

    improve ruby sieve
    replace while loop with reduce and select

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,13 +5,10 @@ class Sieve
 
   def primes
     return [] if @limit < 2
-    primes = []
-    nums = (2..@limit).to_a
-    while nums.count >= 1
-      current_prime = nums.first
-      primes << current_prime
-      nums.each { |n| nums.delete(n) if n % current_prime == 0 }
+    (2..@limit).reduce([]) do |primes, num|
+      divisors = primes.select { |p| num % p == 0 }
+      primes << num if divisors.count == 0
+      primes
     end
-    primes
   end
 end
