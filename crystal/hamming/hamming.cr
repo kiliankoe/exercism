c33a7c77aa2118004739304eaaa69e02ccc3a815
Kilian Koeltzsch <me@kilian.io>
2016-10-05 14:20:36 +0200

    improve crystal hamming
    add type constraints to method params

--- crystal/hamming/hamming.cr
+++ crystal/hamming/hamming.cr
@@ -1,5 +1,5 @@
 module Hamming
-  def self.compute(strand_a, strand_b)
+  def self.compute(strand_a : String, strand_b : String)
     raise ArgumentError.new unless strand_a.chars.size == strand_b.chars.size
 
     strand_a.each_char.with_index.count do |char, idx|
