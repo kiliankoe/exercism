80d1f4ae6c9cb9c119ad8f04c4322417eb6b9972
Kilian Koeltzsch <me@kilian.io>
2017-03-12 18:14:02 +0100

    improve swift strain

--- swift/strain/Sources/Strain.swift
+++ swift/strain/Sources/Strain.swift
@@ -1,20 +1,19 @@
-extension Array {
-    func keep(_ predicate: (Element) -> Bool) -> Array<Element> {
+extension Collection {
+    func keep(_ predicate: (Self.Iterator.Element) -> Bool) -> Array<Self.Iterator.Element> {
         // The following would also work, since it's basically the same idea, but that probably isn't the purpose of this exercism.
         // return self.filter(predicate)
 
-        var new = Array<Element>()
+        var new = Array<Self.Iterator.Element>()
         for e in self {
             if predicate(e) {
                 new.append(e)
             }
         }
         return new
-
     }
 
-    func discard(_ predicate: (Element) -> Bool) -> Array<Element> {
-        var new = Array<Element>()
+    func discard(_ predicate: (Self.Iterator.Element) -> Bool) -> Array<Self.Iterator.Element> {
+        var new = Array<Self.Iterator.Element>()
         for e in self {
             if !predicate(e) {
                 new.append(e)
