b669e59d8ea74773d7ef104dd6a93e72f02e6d29
Kilian Koeltzsch <me@kilian.io>
2017-05-07 01:11:13 +0200

    ecmascript leap

---
+++ ecmascript/leap/leap.js
@@ -0,0 +1,9 @@
+export default class Year {
+    constructor(year) {
+        this.year = year;
+    }
+
+    isLeap() {
+        return this.year % 4 == 0 && this.year % 100 != 0 || this.year % 400 == 0
+    }
+}
---
+++ ecmascript/leap/leap.spec.js
@@ -0,0 +1,25 @@
+import Year from './leap';
+
+describe('A leap year', () => {
+
+  it('year not divisible by 4: common year', () => {
+    let year = new Year(2015);
+    expect(year.isLeap()).toBeFalsy();
+  });
+
+  it('year divisible by 4, not divisible by 100: leap year', () => {
+    let year = new Year(2016);
+    expect(year.isLeap()).toBeTruthy();
+  });
+
+  it('year divisible by 100, not divisible by 400: common year', () => {
+    let year = new Year(2100);
+    expect(year.isLeap()).toBeFalsy();
+  });
+
+  it('year divisible by 400: leap year', () => {
+    let year = new Year(2000);
+    expect(year.isLeap()).toBeTruthy();
+  });
+
+});
