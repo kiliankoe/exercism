aa4bf45e65d741f93a65d3c06c42b1b63d9a6f05
Kilian Koeltzsch <me@kilian.io>
2017-05-06 12:21:33 +0200

    improve rust bob
    - rename QueryKind to PhraseKind
    - use Rust's From trait instead of previous classify()
    - move "logic" into enum as well (reply()), minifying the public function
    - use .to_string().pop() instead of .chars().last(), not sure if better or not

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,29 +1,35 @@
-pub fn reply(query: &str) -> &str {
-    let kind = QueryKind::classify(query);
-    match kind {
-        QueryKind::Normal   => "Whatever.",
-        QueryKind::Shout    => "Whoa, chill out!",
-        QueryKind::Question => "Sure.",
-        QueryKind::Empty    => "Fine. Be that way!",
-    }
+pub fn reply(phrase: &str) -> String {
+    PhraseKind::from(phrase).reply()
 }
 
-enum QueryKind {
+enum PhraseKind {
     Normal,
     Shout,
     Question,
     Empty
 }
 
-impl QueryKind {
-    fn classify(query: &str) -> QueryKind {
-        if query.is_empty() {
-            return QueryKind::Empty
-        } else if query == query.to_uppercase() {
-            return QueryKind::Shout
-        } else if query.chars().last() == Some('?') {
-            return QueryKind::Question
+impl PhraseKind {
+    fn reply(self) -> String {
+        match self {
+            PhraseKind::Normal   => "Whatever.",
+            PhraseKind::Shout    => "Whoa, chill out!",
+            PhraseKind::Question => "Sure.",
+            PhraseKind::Empty    => "Fine. Be that way!",
         }
-        QueryKind::Normal
+        .to_string()
+    }
+}
+
+impl<'a> From<&'a str> for PhraseKind {
+    fn from(phrase: &str) -> PhraseKind {
+        if phrase.is_empty() {
+            return PhraseKind::Empty
+        } else if phrase == phrase.to_uppercase() {
+            return PhraseKind::Shout
+        } else if phrase.to_string().pop() == Some('?') {
+            return PhraseKind::Question
+        }
+        PhraseKind::Normal
     }
 }
