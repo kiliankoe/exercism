95ee4ec77e152612a8ff67034b818cacd095bdf2
Kilian Koeltzsch <me@kilian.io>
2018-04-08 00:19:43 +0200

    improve cpp hello-world

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -1,7 +1,9 @@
-#include <string>
+#include "hello_world.h"
 
 namespace hello_world {
-    std::string hello() {
-        return "Hello, World!";
-    }
+
+std::string hello() {
+    return "Hello, World!";
+}
+
 }
